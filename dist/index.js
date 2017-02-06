edb1df8d48ff353ae991e6afd9853cd4cafa9b6b
Myles O'Connor <myles@gumgum.com>
2017-02-06 14:29:02 -0800

    Access version number from URL params, and display in template

--- dist/index.js
+++ dist/index.js
@@ -20,17 +20,14 @@ var _bodyParser = require('body-parser');
 
 var _bodyParser2 = _interopRequireDefault(_bodyParser);
 
-var _http = require('http');
+var _debug = require('debug');
 
-var _http2 = _interopRequireDefault(_http);
-
-var _reload = require('reload');
-
-var _reload2 = _interopRequireDefault(_reload);
+var _debug2 = _interopRequireDefault(_debug);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var app = (0, _express2.default)();
+var app = (0, _express2.default)(),
+    debugDev = (0, _debug2.default)('dev');
 
 app.set('views', 'views');
 app.set('view engine', 'ejs');
@@ -42,14 +39,17 @@ app.use((0, _cookieParser2.default)());
 app.use(_express2.default.static('public'));
 app.use((0, _serveFavicon2.default)('favicon.ico'));
 
-app.get('/', function (req, res) {
-    res.render('pages/index');
+// Routes
+app.get('/:version', function (req, res) {
+    res.render('pages/index', { versionNumber: req.params.version });
+});
+
+app.post('/:version/generate-html', function (req, res) {
+    debugDev('Received POST request');
+    debugDev(req.params.version);
 });
 
 var port = 8080;
 
-var server = _http2.default.createServer(app);
-(0, _reload2.default)(server, app);
-
 app.listen(port);
-console.log('App running on port ' + port + '.');
+debugDev('App running on port ' + port + '.');
