58e71439d732d575b7b42fbca52c8c574212d61d
Myles O'Connor <myles@gumgum.com>
2017-02-08 14:44:58 -0800

    Built, and fixed test

--- test/index.js
+++ test/index.js
@@ -5,7 +5,7 @@ import '../lib/index.js';
 
 describe('Design System Docs Node Server', () => {
     it('should return 200', done => {
-        http.get('http://127.0.0.1:8080', res => {
+        http.get('http://127.0.0.1:8080/1.2.3', res => {
             assert.equal(200, res.statusCode);
             done();
         });
